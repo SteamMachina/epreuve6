fc40ae4330751d782c3cd8d158b902b1efb183b6
SteamMachina <lisaminne123@gmail.com>
2026-04-01 14:47:54 +0200

    feat:ok to okk

--- vitalsync/backend/server.js
+++ vitalsync/backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/health', (req, res) => {
-    res.json({status: 'ok', time: new Date()});
+    res.json({status: 'okk', time: new Date()});
 });
 
 app.get("/api/activities", (req, res) => {
