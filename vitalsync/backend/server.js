733b119aab357bda5f046c0c2d059f5ef1177f06
SteamMachina <lisaminne123@gmail.com>
2026-04-01 13:58:03 +0200

    init:initialisation du projet

---
+++ vitalsync/backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/health', (req, res) => {
+    res.json({status: 'ok', time: new Date()});
+});
+
+app.get("/api/activities", (req, res) => {
+    res.json([]);
+});
+
+app.listen(3000, () => console.log("VItalSync API on : 3000"));
